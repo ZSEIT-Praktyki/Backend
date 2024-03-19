ab06781b75628c4b0613263d3195e6d149111ea9
DMQQ <kozakdamian73@gmail.com>
2024-03-19 21:28:34 +0100

    env fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,28 @@ import { AppMiddleware } from './app.middleware';
 import { OrdersModule } from './orders/orders.module';
 
 import { WatchlistModule } from './watchlist/watchlist.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from 'process';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: 'shop_praktyki',
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: 'shop_praktyki',
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
     ListingsModule,
